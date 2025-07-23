761849203a032f4ea5a89e7f3179a38336ec9abc
Rayen Selmen <selmen.rayen.92@gmail.com>
2025-07-23 11:49:23 +0100

    Update NotificationIntentAdapter.java

--- lib/android/app/src/main/java/com/wix/reactnativenotifications/core/NotificationIntentAdapter.java
+++ lib/android/app/src/main/java/com/wix/reactnativenotifications/core/NotificationIntentAdapter.java
@@ -12,20 +12,17 @@ import com.wix.reactnativenotifications.core.notification.PushNotificationProps;
 public class NotificationIntentAdapter {
     private static final String PUSH_NOTIFICATION_EXTRA_NAME = "pushNotification";
 
-    @SuppressLint("UnspecifiedImmutableFlag")
-    public static PendingIntent createPendingNotificationIntent(Context appContext, PushNotificationProps notification) {
-        if (canHandleTrampolineActivity(appContext)) {
-            Intent intent = new Intent(appContext, ProxyService.class);
-            intent.putExtra(PUSH_NOTIFICATION_EXTRA_NAME, notification.asBundle());
-            return PendingIntent.getService(appContext, (int) System.currentTimeMillis(), intent, PendingIntent.FLAG_ONE_SHOT);
-        } else {
-            Intent mainActivityIntent = appContext.getPackageManager().getLaunchIntentForPackage(appContext.getPackageName());
-            mainActivityIntent.putExtra(PUSH_NOTIFICATION_EXTRA_NAME, notification.asBundle());
-            TaskStackBuilder taskStackBuilder = TaskStackBuilder.create(appContext);
-            taskStackBuilder.addNextIntentWithParentStack(mainActivityIntent);
-            return taskStackBuilder.getPendingIntent((int) System.currentTimeMillis(), PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_ONE_SHOT | PendingIntent.FLAG_IMMUTABLE);
-        }
-    }
+@SuppressLint("UnspecifiedImmutableFlag")
+public static PendingIntent createPendingNotificationIntent(Context appContext, PushNotificationProps notification) {
+    Intent intent = appContext.getPackageManager().getLaunchIntentForPackage(appContext.getPackageName());
+    intent.putExtra(PUSH_NOTIFICATION_EXTRA_NAME, notification.asBundle());
+    return PendingIntent.getActivity(
+        appContext,
+        (int) System.currentTimeMillis(),
+        intent,
+        PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_ONE_SHOT | PendingIntent.FLAG_IMMUTABLE
+    );
+}
 
     public static boolean canHandleTrampolineActivity(Context appContext) {
         return android.os.Build.VERSION.SDK_INT < android.os.Build.VERSION_CODES.R || appContext.getApplicationInfo().targetSdkVersion < 31;
